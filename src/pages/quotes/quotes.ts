49a98b5423792474fe43d35d6ea71ec17b7d6e3e
Gento <a551.return@gmail.com>
2019-02-09 20:30:16 +0900

    add alert buttons

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -25,7 +25,22 @@ export class QuotesPage implements OnInit{
     const alert = this.alertCtrl.create({
       title: 'Add Quote',
       subTitle: 'Are you sure?',
-      message: 'Are you sure you want to add the quote?'
+      message: 'Are you sure you want to add the quote?',
+      buttons: [
+        {
+          text: 'Yes, go ahead',
+          handler: () => {
+            console.log('OK');
+          }
+        },
+        {
+          text: 'No, I changed my mind',
+          role: 'cancel',
+          handler: () => {
+            console.log('No');
+          }
+        }
+      ]
     });
 
     alert.present();
